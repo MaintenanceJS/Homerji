9ee888f752a43d542c0511e51695dde0b243677b
BatoolAlissa <43234063+BatoolAlissa@users.noreply.github.com>
2018-12-13 14:01:02 +0200

    fix home

--- react-client/src/components/NavBar.jsx
+++ react-client/src/components/NavBar.jsx
@@ -1,55 +1,55 @@
-// import React from "react";
-// import WorkersLogo from "./workersLogo.jsx";
-// import SearchByName from "./workers.jsx";
-// import axios from "axios";
-// import $ from "jquery";
-// import {
-//  Navbar,
-//  Nav,
-//  NavItem,
-//  FormGroup,
-//  FormControl,
-//  Button
-// } from "react-bootstrap"; // For Designing
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import ContactUs from "./contactUs.jsx";
-// import About from "./About.jsx";
-// import Sign from "./Signup.jsx";
+import React from "react";
+import WorkersLogo from "./workersLogo.jsx";
+import SearchByName from "./workers.jsx";
+import axios from "axios";
+import $ from "jquery";
+import {
+ Navbar,
+ Nav,
+ NavItem,
+ FormGroup,
+ FormControl,
+ Button
+} from "react-bootstrap"; // For Designing
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ContactUs from "./contactUs.jsx";
+import About from "./About.jsx";
+import Sign from "./Signup.jsx";
 
 
 
-// class NavBar extends React.Component {
-//  constructor(props) {
-//    super(props);
-//    this.state = {
-//      name: "", //search bar
-//      workers: [] //searched workers
-//    };
-//  }
+class NavBar extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     name: "", //search bar
+     workers: [] //searched workers
+   };
+ }
 
-//  // For the search
-//  getWorkersByName() {
-//    var that = this;
-//    axios.post("/name", { name: this.state.name }).then(function (res) {
-//      that.setState({
-//        workers: res.data
-//      });
-//      $('#textInbox').val('') //to empty the input box
-//      $('#logos').hide() //categories buttons on index file
-//    });
-//  }
+ // For the search
+ getWorkersByName() {
+   var that = this;
+   axios.post("/name", { name: this.state.name }).then(function (res) {
+     that.setState({
+       workers: res.data
+     });
+     $('#textInbox').val('') //to empty the input box
+     $('#logos').hide() //categories buttons on index file
+   });
+ }
 
-//   //get the value of the search bar
-//   getUserName(e) {
-//     this.setState({
-//       name: e.target.value //search bar value
-//     });
-//   }
+  //get the value of the search bar
+  getUserName(e) {
+    this.setState({
+      name: e.target.value //search bar value
+    });
+  }
 
-//   hideCategories() {
-//     console.log('here')
-//     $('#logos').hide() //categories buttons on index file
-//   }
+  hideCategories() {
+    console.log('here')
+    $('#logos').hide() //categories buttons on index file
+  }
   
   render() {
     return (
@@ -101,9 +101,8 @@
         <Route path="/contact" component={ContactUs}/>
         <Route path="/about" component={About}/>
         
-//       </div>
-//     </Router>)
-//   }
-// }
-
-// export default NavBar;
+    </div>
+  </Router>)
+  }
+}
+ export default NavBar;
